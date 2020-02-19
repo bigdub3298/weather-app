46aad013c8377fe646858b070d93b91236fef4ca
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-19 16:52:15 -0700

    got users coordinates

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function WeatherDisplay() {
-  return <div>WeatherDisplay</div>;
+  const [coords, setCoords] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getCoords = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude: lat, longitude: lon } = position.coords;
+        setCoords({ lat, lon });
+      },
+      err => {
+        setErrorMessage(err.message);
+      }
+    );
+  };
+
+  useEffect(() => {
+    getCoords();
+  }, []);
+
+  const renderContent = () => {
+    if (errorMessage) {
+      return <div>Error: {errorMessage}</div>;
+    } else if (coords) {
+      return (
+        <div>
+          <h1>Lat: {coords.lat}</h1>
+          <h1>Lat: {coords.lon}</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="loader">
+          <div className="loader__content">Loading...</div>
+        </div>
+      );
+    }
+  };
+
+  return <div className="weather-display">{renderContent()}</div>;
 }
