452496d7852375f59681332e0ca83e14891bd8b6
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-19 15:41:05 -0700

    displayed weather display in app and tested rendering for app

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import WeatherDisplay from "components/WeatherDisplay/WeatherDisplay";
+
+export default function App() {
+  return (
+    <div>
+      <WeatherDisplay />
+    </div>
+  );
+}
