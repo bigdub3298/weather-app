8f33e96978efd42ea9335704e9f025d66f253285
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-20 11:04:39 -0700

    added node-sass and styled app component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WeatherDisplay from "components/WeatherDisplay/WeatherDisplay";
+import "./App.scss";
+
+export default function App() {
+  return (
+    <div>
+      <WeatherDisplay />
+    </div>
+  );
+}
