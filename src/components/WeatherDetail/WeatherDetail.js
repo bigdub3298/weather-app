819fb5c96f4c3550348c3855eb74630dba03bad5
bigdub3298 <wesleyaustin33@gmail.com>
2020-03-07 09:23:23 -0700

    added more information to weather detail

--- src/components/WeatherDetail/WeatherDetail.js
+++ src/components/WeatherDetail/WeatherDetail.js
@@ -8,6 +8,25 @@ const dateFormatOptions = {
   day: "numeric"
 };
 
+const capitalize = word => {
+  return word
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+const directionFromDegrees = deg => {
+  if (deg < 45 || deg >= 315) {
+    return "N";
+  } else if (deg >= 45 && deg < 135) {
+    return "E";
+  } else if (deg >= 135 && deg < 225) {
+    return "S";
+  } else {
+    return "N";
+  }
+};
+
 export default function WeatherDetail({ weather, date }) {
   const formatHour = hour => {
     const timeAbbreviation = hour > 11 ? "PM" : "AM";
@@ -34,9 +53,16 @@ export default function WeatherDetail({ weather, date }) {
           alt={weather.weather[0].main}
         />
         <div className="weather-detail__body">
+          <h2>{capitalize(weather.weather[0].description)}</h2>
           <p className="weather-detail__currTemp">
             {Math.round(weather.temp.day)}&deg;
           </p>
+          <div className="weather-detail__body-secondary">
+            <p>Humidity: {weather.humidity}%</p>
+            <p>
+              Wind: {weather.speed} mph {directionFromDegrees(weather.deg)}
+            </p>
+          </div>
         </div>
       </div>
     );
