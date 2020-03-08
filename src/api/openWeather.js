ba28f9f24fb7b26ae2d1a0ddde1d72a17383a961
bigdub3298 <wesleyaustin33@gmail.com>
2020-03-08 11:56:12 -0600

    changed baseURL

--- src/api/openWeather.js
+++ src/api/openWeather.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5/forecast"
+  baseURL:
+    " https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast"
 });
